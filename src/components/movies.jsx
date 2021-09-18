e8b7a359ca9026a1db910477a2b4ce5df49c2e5b
Sankar <gsankarsvga@gmail.com>
2021-09-18 05:53:29 +0530

    Learned up to Pagination

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,48 +1,74 @@
-import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
-    state = {
-        movies: getMovies()
-    };
+  state = {
+    movies: getMovies(),
+    pageSize: 4,
+    CurrentPage: 1,
+  };
 
-    handleDelete = (movie) => {
-        const movies = this.state.movies.filter(m => m._id !== movie._id);
-        this.setState({ movies: movies });
-    };
-    render() {
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
 
-        if (this.state.movies.length === 0) {
-            return <p>There are no movies in the database.</p>;
-        }
-        return (
-            <React.Fragment>
-                <p>Showing {this.state.movies.length} moives in the database.</p>
-                <table className="table table-striped table-hover">
-                    <caption>List of Movies</caption>
-                    <thead class="thead-dark">
-                        <tr >
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map(movie => (
-                            <tr key={movie._id} class="table-info">
-                                <td >{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
-                            </tr>))}
-                    </tbody>
-                </table>
-            </React.Fragment>
-        );
-    }
+  handlePageChange = (page) => {
+    this.setState({ CurrentPage: page });
+  };
+  render() {
+    if (this.state.movies.length === 0)
+      return <p>There are no movies in the database.</p>;
+
+    const movies = paginate(
+      this.state.movies,
+      this.state.CurrentPage,
+      this.state.pageSize
+    );
+    return (
+      <React.Fragment>
+        <p>Showing {this.state.movies.length} moives in the database.</p>
+        <table className="table table-striped table-hover">
+          <caption>List of Movies</caption>
+          <thead className="thead-dark">
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie._id} className="table-info">
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          itemsCount={this.state.movies.length}
+          pageSize={this.state.pageSize}
+          CurrentPage={this.state.CurrentPage}
+          OnPageChange={this.handlePageChange}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default Movies;
